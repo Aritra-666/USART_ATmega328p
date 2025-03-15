e0fc53852c619444585804e83525cfae4491a7b3
Aritra-666 <aritrapandit67@gmail.com>
2025-03-15 14:23:31 +0530

    fixed the bug

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include "usart.h"
 
 #define F_CLK 16000000UL                //sets the clk frequency to 16MHz
-#define UBRR_VALUE(x) (F_CLK/(16*x)-1)  //Formula to calculate UBRR's value
+#define UBRR_VALUE(x) F_CLK/16/x-1  //Formula to calculate UBRR's value
 
 int usart_init(int x){
 
-    unsigned long int UBRR = UBRR_VALUE(x); 
+    unsigned int UBRR = UBRR_VALUE(x); 
     
-    UBRR0H = (UBRR>>8);   // sets the higher bits
-    UBRR0L = UBRR;        // sets the lower bits
+    UBRR0H = (unsigned char)(UBRR>>8);   // sets the higher bits
+    UBRR0L = (unsigned char)UBRR;        // sets the lower bits
     
     UCSR0C |= (3<<1);      //sets UCSZ01 and UCSZ00 for 8 bit
+ 
     
     UCSR0B |= (3<<3);      //Enable TX and RX
     
@@ -29,19 +30,36 @@ int usart_transmit(char ch){
 
 int main(void){
 
+    char str[] = "Hello World!!";
+
     DDRB |= (1 << 5);
  
     usart_init(9600);  //initialize hardware
     
     while(1){
     
-        for( long int i = 0; i<=50000 ; i++){}
-        PORTB |= (1<<5);   //turn off the led indicator
+        int index;
         
-        usart_transmit('A');  //transmit char 'A'
+        usart_transmit('\r');
         
-        for( long int i = 0; i<=50000 ; i++){}
-        PORTB &= ~(1<<5);    //turn on the led indicator
+        for( index = 0 ; str[index] != '\0' ; index++){
+        
+            PORTB |= (1<<5);                        //turn on the led indicator
+            
+            for( long int i = 0; i<=500000 ; i++){}  //delay
+            
+            usart_transmit(str[index]);                    //transmit char
+            
+            PORTB &= ~(1<<5);                       //turn off the led indicator
+        }
+        
+        for( unsigned long int i = 0; i<=1000000 ; i++){}  //delay
+        
+        index=0;
+        
+        usart_transmit('\n');
+        
+    
     }
  
     return 0;
--- usart.h
+++ usart.h
@@ -1,10 +1,10 @@
-#define PORTB *((volatile unsigned char *)0x25) 
-#define DDRB *((volatile unsigned char *)0x24)
-#define UDR0 *((volatile unsigned char *)0xC6)
-#define UBRR0H *((volatile unsigned char *)0xC5)
-#define UBRR0L *((volatile unsigned char *)0xC4)
-#define UCSR0C *((volatile unsigned char *)0xC2)
-#define UCSR0B *((volatile unsigned char *)0xC1)
-#define UCSR0A *((volatile unsigned char *)0xC0)
+#define PORTB	 *((volatile unsigned char *)0x25) 
+#define DDRB	 *((volatile unsigned char *)0x24)
+#define UDR0 	 *((volatile unsigned char *)0xC6)
+#define UBRR0H 	 *((volatile unsigned char *)0xC5)
+#define UBRR0L 	 *((volatile unsigned char *)0xC4)
+#define UCSR0C 	 *((volatile unsigned char *)0xC2)
+#define UCSR0B 	 *((volatile unsigned char *)0xC1)
+#define UCSR0A 	 *((volatile unsigned char *)0xC0)
 
 
